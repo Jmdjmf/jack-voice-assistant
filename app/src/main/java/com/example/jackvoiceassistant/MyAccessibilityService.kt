9d9e779faaadfc05d14fe588ebf54db79aa0ac85
Xvivek <86772137+Jmdjmf@users.noreply.github.com>
2026-08-01 19:29:52 +0530

    Add logging in onServiceConnected method

--- app/src/main/java/com/example/jackvoiceassistant/MyAccessibilityService.kt
+++ app/src/main/java/com/example/jackvoiceassistant/MyAccessibilityService.kt
@@ -14,6 +14,7 @@ class MyAccessibilityService : AccessibilityService() {
     override fun onServiceConnected() {
         super.onServiceConnected()
         instance = this
+        Logger.log("MyAccessibilityService: onServiceConnected() fired, instance set")
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
